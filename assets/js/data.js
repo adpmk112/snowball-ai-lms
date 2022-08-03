38f7ac6b7180dfb04f0c03e44747747dd9192897
Htet Wai Aung <htetwaiaung1236@gmail.com>
2022-08-03 20:13:32 +0630

    HtetWaiAungFix

---
+++ assets/js/data.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('#datatable').DataTable()
+  });
+  $(document).ready(function () {
+    $('#datatable1').DataTable()
+  });
+  $(document).ready(function () {
+    $('#datatable2').DataTable()
+  });
