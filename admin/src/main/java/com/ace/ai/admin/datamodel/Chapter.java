59655df6612a1bd12691704344274f803c9a427f
adpmk112 <patrickhant642@gmail.com>
2022-08-18 20:13:34 +0630

    chapter_file bean added

--- admin/src/main/java/com/ace/ai/admin/datamodel/Chapter.java
+++ admin/src/main/java/com/ace/ai/admin/datamodel/Chapter.java
@@ -1,22 +1,28 @@
 package com.ace.ai.admin.datamodel;
 
 import javax.persistence.*;
+
+import lombok.Data;
+
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Data
 public class Chapter {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private int id;
     private String name;
+    private boolean delete_status;
 
     @ManyToOne
     @JoinColumn(name = "course_id")
     private Course course;
 
-    private boolean delete_status;
     @OneToMany(mappedBy = "chapter")
-    private List<Chapter_Batch> chapter_batches = new ArrayList<>();
+    private List<Chapter_Batch> chapter_Batches = new ArrayList<>();
 
+    @OneToMany(mappedBy = "chapter")
+    private List<Chapter_File> chapter_Files = new ArrayList<>();
 }
