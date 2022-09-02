a9a24aecb93c8d1b1c96b8599afe0131924ab509
adpmk112 <patrickhant642@gmail.com>
2022-09-02 15:55:30 +0630

    classroomEdit is done

--- admin/src/main/java/com/ace/ai/admin/controller/ErrorController.java
+++ admin/src/main/java/com/ace/ai/admin/controller/ErrorController.java
@@ -1,26 +1,25 @@
-package com.ace.ai.admin.controller;
+// package com.ace.ai.admin.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Conditional;
-import org.springframework.http.HttpStatus;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
+// import org.springframework.http.HttpStatus;
+// import org.springframework.ui.Model;
+// import org.springframework.web.bind.annotation.ControllerAdvice;
+// import org.springframework.web.bind.annotation.ExceptionHandler;
+// import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ControllerAdvice
-public class ErrorController {
-    private Logger logger= LoggerFactory.getLogger(ErrorController.class);
+// @ControllerAdvice
+// public class ErrorController {
+//     private Logger logger= LoggerFactory.getLogger(ErrorController.class);
 
 
-    @ExceptionHandler(Throwable.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public String exception(final Throwable throwable,final Model model){
-        logger.error("Exception during execution of application ", throwable);
-        String errorMessage=(throwable !=null ? throwable.getMessage() :"Unknown error");
-        model.addAttribute("errorMessage",errorMessage);
-        return "ERR01";
-    }
+//     @ExceptionHandler(Throwable.class)
+//     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+//     public String exception(final Throwable throwable,final Model model){
+//         logger.error("Exception during execution of application ", throwable);
+//         String errorMessage=(throwable !=null ? throwable.getMessage() :"Unknown error");
+//         model.addAttribute("errorMessage",errorMessage);
+//         return "ERR01";
+//     }
 
-}
+// }
