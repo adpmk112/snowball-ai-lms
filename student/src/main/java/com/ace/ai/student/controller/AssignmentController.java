7268b48f561733ba05eea3578ed95060ff2dc6ec
WinWinKhaing2022 <win242win.khaing@gmail.com>
2022-09-16 10:23:55 +0630

    studentAssignment

--- student/src/main/java/com/ace/ai/student/controller/AssignmentController.java
+++ student/src/main/java/com/ace/ai/student/controller/AssignmentController.java
@@ -1,14 +1,85 @@
 package com.ace.ai.student.controller;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import com.ace.ai.student.datamodel.StudentAssignmentMark;
+import com.ace.ai.student.dtomodel.AssignmentDateTimeDTO;
+import com.ace.ai.student.dtomodel.AssignmentFileDTO;
+import com.ace.ai.student.dtomodel.StudentMarkDTO;
+import com.ace.ai.student.repository.StudentAssignmentMarkRepository;
+import com.ace.ai.student.service.AssignmentService;
 
 @Controller
+@RequestMapping("/student")
 public class AssignmentController {
 
-    @GetMapping("/a")
-    public String assignmentStudent(){
-        return "S001-03";
+    @Autowired
+    AssignmentService assignmentService;
+    @Autowired
+    StudentAssignmentMarkRepository studentAssignmentMarkRepository;
+
+    @GetMapping("/assignmentView")
+    public ModelAndView assignmentStudent(@RequestParam("assignmentId") int assignmentId,@RequestParam("studentId") int studentId,@RequestParam("chapterId") int chapterId,ModelMap model){
+        AssignmentDateTimeDTO assignmentDateTimeDTO = assignmentService.getDateTimeByAssignmentId(assignmentId);
+        StudentMarkDTO studentMarkDTO = assignmentService.getStudentMarkByAssiIdAndStuId( assignmentId, studentId);
+        model.addAttribute("assignmentDateTimeDTO" ,assignmentDateTimeDTO);
+        model.addAttribute("studentMarkDTO", studentMarkDTO);
+        return new ModelAndView("S001-03","assignmentFileDTO",new AssignmentFileDTO());
+    }
+
+    @PostMapping("/assignmentAdd")
+    public String assignmentAdd(@ModelAttribute("assignmentFileDTO") AssignmentFileDTO assignmentFileDTO, ModelMap model){
+        LocalDate localDate = LocalDate.now();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String localDateString = localDate.format(dateTimeFormatter);
+        LocalTime localTime = LocalTime.now();
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("hh:mm:ss a");
+        String currentTime = localTime.format(timeFormatter);
+        StudentAssignmentMark  studentAssignmentMark = new StudentAssignmentMark();
+        String fileName=assignmentFileDTO.getAssignmentFile().getOriginalFilename();
+        studentAssignmentMark.setUploadedFile(fileName);
+        studentAssignmentMark.setDate(localDateString);
+        studentAssignmentMark.setTime(currentTime );
+        StudentAssignmentMark studentAssignmentMarkSaved = studentAssignmentMarkRepository.save(studentAssignmentMark);
+        
+        Path uploadPath = Paths.get("./assets/img/assignmentFiles/"+studentAssignmentMarkSaved.getId());
+        if(!Files.exists(uploadPath)){
+            try {
+              Files.createDirectories(uploadPath);
+            } catch (IOException e) {
+              
+              e.printStackTrace();
+            }
+            }
+          try( InputStream inputStream=assignmentFileDTO.getAssignmentFile().getInputStream()){
+            Path filePath=uploadPath.resolve(assignmentFileDTO.getAssignmentFile().getOriginalFilename());
+            System.out.println(filePath.toFile().getAbsolutePath());
+            Files.copy(inputStream, filePath ,StandardCopyOption.REPLACE_EXISTING);
+          }catch (IOException e){
+              try {
+                throw new IOException("Could not save upload file: " + assignmentFileDTO.getAssignmentFile().getOriginalFilename());
+              } catch (IOException e1) {
+                
+                e1.printStackTrace();
+              }
+          } 
+          return "redirect:/teacher/assignmentView";
     }
     
 }
---
+++ student/src/main/java/com/ace/ai/student/service/AssignmentService.java
@@ -0,0 +1,43 @@
+package com.ace.ai.student.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ace.ai.student.datamodel.Assignment;
+import com.ace.ai.student.datamodel.StudentAssignmentMark;
+import com.ace.ai.student.dtomodel.AssignmentDateTimeDTO;
+import com.ace.ai.student.dtomodel.StudentMarkDTO;
+import com.ace.ai.student.repository.AssignmentRepository;
+import com.ace.ai.student.repository.StudentAssignmentMarkRepository;
+
+
+@Service
+public class AssignmentService {
+
+    @Autowired
+    AssignmentRepository assignmentRepository;
+    @Autowired
+    StudentAssignmentMarkRepository studentAssignmentMarkRepository;
+    
+    public AssignmentDateTimeDTO getDateTimeByAssignmentId(int assignmentId){
+        Assignment assignment = assignmentRepository.findById(assignmentId);
+        AssignmentDateTimeDTO assignmentDateTimeDTO = new AssignmentDateTimeDTO();
+        String end_date = assignment.getEnd_date();
+        String end_time = assignment.getEnd_time();
+        assignmentDateTimeDTO.setEnd_date(end_date);
+        assignmentDateTimeDTO.setEnd_time(end_time);
+        return assignmentDateTimeDTO;
+    }
+
+    public StudentMarkDTO getStudentMarkByAssiIdAndStuId(int assignmentId, int studentId){
+        StudentAssignmentMark studentAssignmentMark =studentAssignmentMarkRepository.findByAssignmentIdAndStudentId(assignmentId , studentId);
+        StudentMarkDTO studentMarkDTO = new StudentMarkDTO();
+        studentMarkDTO.setStudentMark(studentAssignmentMark.getStudentMark());
+        return studentMarkDTO;
+    }
+
+    }
+
