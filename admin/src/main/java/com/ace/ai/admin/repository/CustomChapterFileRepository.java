a00f778e319509e9e3c2e4f5108d2686abd4ca10
Pmk <77253810+adpmk112@users.noreply.github.com>
2022-09-21 03:31:47 +0630

    customChapterAssignment process

--- admin/src/main/java/com/ace/ai/admin/repository/CustomChapterFileRepository.java
+++ admin/src/main/java/com/ace/ai/admin/repository/CustomChapterFileRepository.java
@@ -4,11 +4,10 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import com.ace.ai.admin.datamodel.CustomChapter;
 import com.ace.ai.admin.datamodel.CustomChapterFile;
 
 @Repository
 public interface CustomChapterFileRepository extends JpaRepository<CustomChapterFile,Integer>{
     List<CustomChapterFile> findByCustomChapterIdAndDeleteStatus(int customChapterId,boolean deleteStatus);
+    List<CustomChapterFile> findByCustomChapterIdAndFileTypeAndDeleteStatus(int customChapterId,String fileType,boolean deleteStatus);
 }
